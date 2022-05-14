95e5a5b2a4c48d56045757d7ede3ed3d1f7e7cd4
shockylemi <shockylemi@gmail.com>
2022-05-14 17:13:01 +0430

    swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,30 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 
+  // standard
   const app = await NestFactory.create(AppModule);
+
+
+  // global prefix
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Boghche API')
+    .setDescription('Documentation for all endpoints')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+  
+  // run server
   const port = process.env.PORT || 3333;
   await app.listen(port);
 
