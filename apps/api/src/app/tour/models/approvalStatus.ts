77bf3ec84c7f048b19a517338cadca844bde0194
shockylemi <shockylemi@gmail.com>
2022-05-21 14:14:26 +0430

    approval status for tours

---
+++ apps/api/src/app/tour/models/approvalStatus.ts
@@ -0,0 +1,8 @@
+export enum ApprovalStatus {
+  /** منتظر بررسی */
+  pending = 0,
+  /** تایید شده */
+  accepted = 1,
+  /** رد شده */
+  rejected = 2 
+}
--- apps/api/src/app/tour/models/tour.entity.ts
+++ apps/api/src/app/tour/models/tour.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, CreateDateColumn } from 'typeorm';
+import { ApprovalStatus } from './approvalStatus';
 
 @Entity()
 export class Tour extends BaseEntity {
@@ -7,33 +8,40 @@ export class Tour extends BaseEntity {
 
   @CreateDateColumn()
   dateCreated: Date = new Date()
-  
+
   @Column()
   title: string
-  
+
   @Column({ nullable: true })
   description: string = undefined
-  
+
   @Column({ nullable: true })
   bannerUrl: string = undefined
-  
+
   @Column("simple-array", { array: false })
   galleryUrls: string[] = []
-  
+
   /** مدت زمان */
   @Column()
   duration: Date
-  
+
   @Column()
   price: number
-  
+
   @Column()
   capacity: number
-  
+
   /** امتیاز ها */
   @Column()
   ratings: number = 0
-  
+
+  @Column({
+    type: 'enum',
+    enum: [0, 1, 2],
+    default: 0
+  })
+  approvalStatus: ApprovalStatus = 0
+
   // @Column()
   // comments: Comment[]
 }
